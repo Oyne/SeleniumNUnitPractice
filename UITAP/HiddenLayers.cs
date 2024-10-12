258c687ba88e9c89873b7d231708cf0e1bcdafb5
Oyne <andreoyy.litvinovv@gmail.com>
2024-10-13 00:45:40 +0300

    Added chromeOptions

--- UITAP/HiddenLayers.cs
+++ UITAP/HiddenLayers.cs
@@ -10,7 +10,9 @@ namespace UITAP
         public void Setup()
         {
             ChromeOptions chromeOptions = new ChromeOptions();
-
+            chromeOptions.AddArgument("--headless"); // Run Chrome in headless mode
+            chromeOptions.AddArgument("--no-sandbox"); // Bypass OS security model
+            chromeOptions.AddArgument("--disable-dev-shm-usage"); // Overcome limited resource issue
             _driver = new ChromeDriver(chromeOptions);
             _driver.Navigate().GoToUrl("http://uitestingplayground.com/hiddenlayers");
         }
