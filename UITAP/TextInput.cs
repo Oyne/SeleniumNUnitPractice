f0fc7496ca72e3e029f394a66122c06dbc10ab80
Oyne <andreoyy.litvinovv@gmail.com>
2024-10-13 22:46:34 +0300

    Text Input test added

---
+++ UITAP/TextInput.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UITAP
+{
+    internal class TextInput
+    {
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            //chromeOptions.AddArgument("--headless"); // Run Chrome in headless mode
+            //chromeOptions.AddArgument("--no-sandbox"); // Bypass OS security model
+            //chromeOptions.AddArgument("--disable-dev-shm-usage"); // Overcome limited resource issue
+            _driver = new ChromeDriver(chromeOptions);
+            _driver.Navigate().GoToUrl("http://uitestingplayground.com/textinput?");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Dispose();
+        }
+
+        [Test]
+        public void TextInput_Test()
+        {
+            var driver = _driver;
+            string inputFieldXpath = "//*[@id=\"newButtonName\"]";
+            string buttonXpath = "//*[@id=\"updatingButton\"]";
+            IWebElement inputField = driver.FindElement(By.XPath(inputFieldXpath));
+            IWebElement button = driver.FindElement(By.XPath(buttonXpath));
+            inputField.SendKeys("Test");
+            button.Click();
+            Assert.That(button.Text == "Test");
+        }
+    }
+}
